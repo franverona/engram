5421f531bad59a011aedfb35b7e59075effb0f09
Fran Verona <franverona@gmail.com>
2026-02-19 11:39:09 +0000

    feat: register chats router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from '../init'
+import { chatsRouter } from './chats'
 import { notesRouter } from './notes'
 import { searchRouter } from './search'
 
 export const appRouter = createTRPCRouter({
   notes: notesRouter,
   search: searchRouter,
+  chats: chatsRouter,
 })
 
 export type AppRouter = typeof appRouter
